688911cf93ec70ef8f59cef6d5f79ba27e8767a6
Raphael Kai <584520kailin@gmail.com>
2025-06-23 23:57:27 +0800

    record:组件通信

---
+++ Vue/Communication/Communication/README.md
@@ -0,0 +1,3 @@
+### provide 需要制定一个键(Symble()) 第二个参数为需要向子孙组件提供的数据 inject 使用这个唯一键接收
+
+### v-model 是绑定一个值，子组件通过defineProps接收，修改时需使用defineEmit发送更新
---
+++ Vue/Communication/Communication/src/components/Child1.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+	message:String
+})
+</script>
+<template>
+  <div>
+	<button @click="()=>console.log('--',props.message)"
+	>click</button>
+	<div>{{ props }}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
