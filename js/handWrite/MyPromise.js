bc5d42a9fbfbe90bed6bb8ab45936524b1123f88
KaiueY <584520kailin@gmail.com>
2025-07-28 18:30:14 +0800

    test

--- js/handWrite/MyPromise.js
+++ js/handWrite/MyPromise.js
@@ -85,7 +85,6 @@ class MyPromise {
 		promises = [...promises]
 		const result = []
 		let count = 0
-
 		return new Promise((resolve, reject) => {
 			if (promises.length === 0) resolve(result)
 			promises.forEach((promise, index) => {
@@ -228,5 +227,4 @@ class MyPromise {
 
 MyPromise.reject(221).then(null, err => {
 	console.log(err, 'err');
-
 })
